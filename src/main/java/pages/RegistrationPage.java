667325f45b999749e78933814aed407d4f986199
Raveendra <ravichintin@gmail.com>
2026-07-26 22:04:43 +0530

    adding the locators for the RegistrationPage

--- src/main/java/pages/RegistrationPage.java
+++ src/main/java/pages/RegistrationPage.java
@@ -1,9 +1,40 @@
 package pages;
 
-import base.BaseClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 
-public class RegistrationPage extends BaseClass{
+import base.BasePage;
+
+public class RegistrationPage extends BasePage
+{
+	public RegistrationPage(WebDriver driver)
+	{
+		super(driver);
+	}
 	
+		private By name = By.xpath("//input[@placeholder='Name']");
+		private By email = By.xpath("//input[@placeholder='Email']");
+		private By password = By.xpath("//input[@placeholder='Password']");
+		
+		// Interests checkboxes
+		private By jmeter = By.xpath("//label[text()='JMeter']/preceding-sibling::*");
+		private By java = By.xpath("//label[text()='Java']/preceding-sibling::*");
+		private By JavaScript = By.xpath("//label[text()='JavaScript']/preceding-sibling::*");
+		private By TestNG = By.xpath("//label[text()='TestNG']/preceding-sibling::*");
+		
+		private By stateDropdown = By.xpath("//select[@name='state']");
+		
+		public void selectState(String state){
+			Select select=new Select(driver.findElement(stateDropdown));
+			select.selectByVisibleText(state);
+		}
+		
+		
+		
+		
+		
+	    
 
 
 	
